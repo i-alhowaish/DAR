41bdd43c5a51df847e95fa662e6ca604fcf506d3
AlajajiOmar <omar.151.ksa@gmail.com>
2024-03-29 22:40:54 +0300

    adding map in add property page

---
+++ DAR/static/js/add-property-map.js
@@ -0,0 +1,26 @@
+var markerCoordinates = [24.686708, 46.572200];
+var lat = markerCoordinates[0];
+var lng = markerCoordinates[1];
+
+var map = L.map('map').setView(markerCoordinates, 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">DAR</a>'
+}).addTo(map);
+
+
+
+var marker = L.marker(markerCoordinates).addTo(map).bindPopup(`اذهب الى <a href="https://www.google.com/maps?q=${lat}, ${lng}" target="_blank">قوقل ماب</a>`)
+
+
+L.control.locate().addTo(map);
+
+
+
+function openGoogleMaps() {
+  var lat = markerCoordinates[0];
+  var lng = markerCoordinates[1];
+  window.open('https://www.google.com/maps?q=' + lat + ',' + lng, '_blank');
+}
+
+
